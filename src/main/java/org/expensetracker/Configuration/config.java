18c2a59875edaaff8001148d358d88b051b982f0
Dev Sengar <premlatasengar31@gmail.com>
2026-06-11 20:37:34 +0530

    adding config file

---
+++ src/main/java/org/expensetracker/Configuration/config.java
@@ -0,0 +1,15 @@
+package org.expensetracker.Configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class config {
+
+    @Bean
+public ModelMapper modelMapper(){
+    return new ModelMapper();
+}
+
+}
